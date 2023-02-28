a91e81d51f4fc2b359a40669a3304577fffa1239
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-02-27 17:39:44 -0800

    configured Loader component

--- social-blog/pages/index.tsx
+++ social-blog/pages/index.tsx
@@ -3,17 +3,14 @@ import Link from 'next/link';
 import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
 
+import Loader from '../components/Loader'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div>
-      <Link prefetch={false} href={{
-        pathname: '/[username]',
-        query: { username: 'csanchbach' }
-      }}>
-        <p>Colby's profile</p>
-      </Link>
+      <Loader show/>
     </div>
   )
 }
