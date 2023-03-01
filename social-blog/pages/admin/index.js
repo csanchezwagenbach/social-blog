43625222a7a614a5fd1c4bf94040bfb6412fe4aa
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-03-01 10:11:20 -0800

    admin page modules imported

--- social-blog/pages/admin/index.js
+++ social-blog/pages/admin/index.js
@@ -1,4 +1,15 @@
+import styles from '../../styles/Admin.module.css';
 import AuthCheck from '../../components/AuthCheck';
+import PostFeed from '../../components/PostFeed';
+import { UserContext } from '../../lib/context';
+import { firestore, auth, serverTimestamp } from '../../lib/firebase';
+
+import { useContext, useState } from 'react';
+import { useRouter } from 'next/router';
+
+import { useCollection } from 'react-firebase-hooks/firestore';
+import kebabCase from 'lodash.kebabcase';
+import toast from 'react-hot-toast';
 
 
 export default function AdminPostsPage(props) {
