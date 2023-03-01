e64fb72f76bbe64799e11fa7dbab531203a43512
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-03-01 15:07:54 -0800

    admin create new post page configured

--- social-blog/pages/admin/index.js
+++ social-blog/pages/admin/index.js
@@ -16,8 +16,76 @@ export default function AdminPostsPage(props) {
     return (
         <main>
             <AuthCheck>
-
+            <PostList />
+            <CreateNewPost />
             </AuthCheck>
         </main>
+    );
+}
+
+function PostList() {
+    const ref = firestore.collection('users').doc(auth.currentUser.uid).collection('posts');
+    const query = ref.orderBy('createdAt');
+    const [querySnapshot] = useCollection(query);
+
+    const posts = querySnapshot?.docs.map((doc) => doc.data());
+
+    return (
+        <>
+        <h1>Manage your Posts</h1>
+        <PostFeed posts={posts} admin />
+        </>
+    )
+}
+
+function CreateNewPost() {
+    const router = useRouter();
+    const { username } = useContext(UserContext);
+    const [title, setTitle] = useState('');
+
+    const slug = encodeURI(kebabCase(title));
+
+    const isValid = title.length > 3 && title.length < 100;
+
+    const createPost = async (e) => {
+        e.prevenDefault();
+        const uid = auth.currentUser.uid;
+        const ref = firestore.collection('users').doc(uid).collection('posts').doc(slug);
+
+        const data = {
+            title,
+            slug,
+            uid,
+            username,
+            published: false,
+            content: '# hello world!',
+            createdAt: serverTimestamp(),
+            updatedAt: serverTimestamp(),
+            heartCount: 0
+        };
+
+        await ref.set(data);
+
+        toast.success('Post created!')
+
+        router.push(`/admin/${slug}`);
+
+    };
+
+    return (
+        <form onSubmit={createPost}>
+            <input
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder='My Awesome Article!'
+                className={styles.input}
+                />
+                <p>
+                    <strong>Slug:</strong> {slug}
+                </p>
+                <button type='submit' disabled={!isValid} className='btn-green'>
+                    Create New Post
+                </button>
+        </form>
     );
 }
